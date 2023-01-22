35c528a60644a5a551db335ec2a2a55090f86052
EduuCarvalho <educarvalho.engcivil@gmail.com>
2023-01-22 15:41:25 -0300

    fix: bug na rota post film solucionado

--- src/middlewares/filmesValidations.ts
+++ src/middlewares/filmesValidations.ts
@@ -1,9 +1,11 @@
 import { NextFunction, Request,Response } from "express";
-import { filmesPostModel } from "../models/filmesModel";
+import { filmesPostModel } from "../models/filmesModel.js";
 
 export async function filmesPostValidations(req: Request,res: Response,next:NextFunction) {
         const { error } = filmesPostModel.validate(req.body,{abortEarly:false});
 
         if(error) return res.status(422).send(error.details.map(detail=> detail.message));
-        
+
+        next()
+
 }   
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express"
 import {getHealth, postFilme} from "../controllers/movies.controllers.js";
+import { filmesPostValidations } from "../middlewares/filmesValidations.js";
 
 const routes = Router();
 
 routes.get("/health", getHealth);
-routes.post("/filme",postFilme)
+routes.post("/filme",filmesPostValidations,postFilme)
 
 export default routes;
